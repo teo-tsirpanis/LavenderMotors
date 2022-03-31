5e1e632a88fde29257056ab8eeb81a3318baec9d
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-03-31 20:29:15 +0300

    Add the transaction entity types.

---
+++ src/LavenderMotors.Entities/Transaction.cs
@@ -0,0 +1,21 @@
+﻿namespace LavenderMotors.Entities;
+
+public class Transaction
+{
+    public Guid Id { get; }
+    public DateTime Date { get; }
+    public Customer Customer { get; }
+    public Car Car { get; }
+    public Manager Manager { get; }
+    public decimal TotalPrice { get; }
+
+    public List<Transaction> Transactions { get; } = new();
+
+    public Transaction(DateTime date, Customer customer, Car car, Manager manager)
+    {
+        Date = date;
+        Customer = customer;
+        Car = car;
+        Manager = manager;
+    }
+}
