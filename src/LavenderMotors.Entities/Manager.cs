7191cc2ac5b6dc0212b994ba7d8ffe93af42561d
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-03-31 20:11:26 +0300

    Add the employee entity types.

---
+++ src/LavenderMotors.Entities/Manager.cs
@@ -0,0 +1,8 @@
+﻿namespace LavenderMotors.Entities;
+
+public class Manager : Employee
+{
+    public Manager (string name, string surname, decimal salaryPerMonth) : base(name, surname, salaryPerMonth)
+    {
+    }
+}
