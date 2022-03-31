4ac9fc1b50ff0b79b848fdc1767aca8205ed8a0a
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-03-31 19:53:40 +0300

    Add the customer-facing entity types.

---
+++ src/LavenderMotors.Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace LavenderMotors.Entities;
+
+public class Customer
+{
+    // The ID will be automatically set by the database.
+    public Guid Id { get; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Phone { get; set; }
+    /// <summary>
+    /// Tax Identification Number
+    /// </summary>
+    /// <remarks>
+    /// In Greece it is known as "ΑΦΜ".
+    /// </remarks>
+    public string TIN { get; set; }
+    
+    public Customer(string name, string surname, string phone, string tin)
+    {
+        Name = name;
+        Surname = surname;
+        Phone = phone;
+        TIN = tin;
+    }
+}
---
+++ src/LavenderMotors.Entities/ServiceTask.cs
@@ -0,0 +1,17 @@
+﻿namespace LavenderMotors.Entities;
+
+public class ServiceTask
+{
+    // The ID will be automatically set by the database.
+    public Guid Id { get; }
+    public string Code { get; set; }
+    public string Description { get; set; }
+    public decimal Hours { get; set; }
+
+    public ServiceTask(string code, string description, decimal hours)
+    {
+        Code = code;
+        Description = description;
+        Hours = hours;
+    }
+}
