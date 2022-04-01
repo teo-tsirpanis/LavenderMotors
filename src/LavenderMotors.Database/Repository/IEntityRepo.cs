280df784ec3bbe4856a12f88f68f617dd92b6d36
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-04-01 20:25:39 +0300

    Add a generic entity repository interface.

---
+++ src/LavenderMotors.Database/Repository/IEntityRepo.cs
@@ -0,0 +1,10 @@
+﻿namespace LavenderMotors.Database.Repository;
+
+public interface IEntityRepo<T>
+{
+    IAsyncEnumerable<T> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(Guid id, T entity);
+    Task DeleteAsync(Guid id);
+}
