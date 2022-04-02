40372dbe67120e2413ea07bf8555f046dbdd598c
Evangelos Mandalis <evanmand@ea.gr>
2022-04-02 15:07:10 +0300

    Add EmployeeRepo.

--- src/LavenderMotors.Entities/Engineer.cs
+++ src/LavenderMotors.Entities/Engineer.cs
@@ -3,13 +3,12 @@
 public class Engineer : Employee
 {
     private Manager? _manager;
-
+    public bool HasManager => _manager is not null;
     public Manager Manager
     {
         get => _manager ?? throw Utilities.CreateUnboundValueAccessException();
         set => _manager = value;
     }
-
     public Guid ManagerId { get; set; }
 
     public Engineer(string name, string surname, decimal salaryPerMonth) : base(name, surname, salaryPerMonth)
