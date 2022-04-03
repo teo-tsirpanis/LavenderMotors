34219745834fb40b7723acee5370083cc29b5e7f
IliasKyropoulos <100684484+IliasKyropoulos@users.noreply.github.com>
2022-04-03 11:14:39 +0300

    Added Car and Service Task web pages. (#12)
    * Added Car web page.
    
    * Added Service Task web page.

---
+++ src/LavenderMotors.Web/Controllers/CarController.cs
@@ -0,0 +1,118 @@
+﻿#nullable disable
+using LavenderMotors.Database.Repository;
+using LavenderMotors.Entities;
+using LavenderMotors.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace LavenderMotors.Web.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly IEntityRepo<Car> _carRepo;
+
+        public CarController(IEntityRepo<Car> carRepo)
+        {
+            _carRepo = carRepo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _carRepo.GetAllAsync());
+        }
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var car = await _carRepo.GetByIdAsync(id.Value);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new CarListViewModel(car)
+            {
+
+            };
+            return View(car);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Brand,Model,CarRegistrationNumber")] CarCreateViewModel carCreateViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var newCar = new Car(carCreateViewModel.Brand, carCreateViewModel.Model, carCreateViewModel.CarRegistrationNumber);
+                await _carRepo.AddAsync(newCar);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carCreateViewModel);
+        }
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var car = await _carRepo.GetByIdAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Brand,Model,CarRegistrationNumber")] CarUpdateViewModel carUpdateViewModel)
+        {
+            if (id != carUpdateViewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var currentCar = await _carRepo.GetByIdAsync(id);
+                if (currentCar is null)
+                    return BadRequest("Could not find car.");
+                currentCar.Brand = carUpdateViewModel.Brand;
+                currentCar.Model = carUpdateViewModel.Model;
+                currentCar.CarRegistrationNumber = carUpdateViewModel.CarRegistrationNumber;
+                await _carRepo.UpdateAsync(id, currentCar);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carUpdateViewModel);
+        }
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var car = await _carRepo.GetByIdAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new CarDeleteViewModel()
+            {
+                Brand = car.Brand,
+                Id = car.Id,
+                Model = car.Model,
+                CarRegistrationNumber = car.CarRegistrationNumber,
+            };
+
+            return View(viewModel);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var car = await _carRepo.GetByIdAsync(id);
+            await _carRepo.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
